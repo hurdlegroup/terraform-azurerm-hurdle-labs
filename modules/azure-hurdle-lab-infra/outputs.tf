6415c9e22bbbb9ace5e8e6d2dff58339f0d864cd
Jacob Drummond <jacob.drummond@hurdle.live>
2026-06-23 11:19:28 +0100

    fix(HRLD-1276): switch outputs to `try(..., null)` instead of breaking on undefined `[0]`

--- modules/azure-hurdle-lab-infra/outputs.tf
+++ modules/azure-hurdle-lab-infra/outputs.tf
@@ -25,7 +25,7 @@ output "machines_subnet_id" {
 
 output "machines_nat_gateway_id" {
   description = "ID of the machines subnet NAT gateway."
-  value       = local.is_nat_mode ? azurerm_nat_gateway.machines[0].id : null
+  value       = try(azurerm_nat_gateway.machines[0].id, null)
 }
 
 output "machines_egress_mode" {
@@ -35,17 +35,17 @@ output "machines_egress_mode" {
 
 output "machines_route_table_id" {
   description = "Route table ID associated with machines subnet when firewall egress is enabled."
-  value       = local.is_nat_mode ? null : (local.use_byo_route_table ? data.azurerm_route_table.byo[0].id : azurerm_route_table.machines_egress[0].id)
+  value       = local.is_nat_mode ? null : (local.use_byo_route_table ? try(data.azurerm_route_table.byo[0].id, null) : try(azurerm_route_table.machines_egress[0].id, null))
 }
 
 output "machines_managed_firewall_id" {
   description = "ID of managed Azure Firewall when machines_egress_mode is firewall_module_provisioned."
-  value       = local.is_managed_firewall_mode ? azurerm_firewall.managed[0].id : null
+  value       = try(azurerm_firewall.managed[0].id, null)
 }
 
 output "machines_managed_firewall_private_ip" {
   description = "Private IP of managed Azure Firewall when machines_egress_mode is firewall_module_provisioned."
-  value       = local.is_managed_firewall_mode ? azurerm_firewall.managed[0].ip_configuration[0].private_ip_address : null
+  value       = try(azurerm_firewall.managed[0].ip_configuration[0].private_ip_address, null)
 }
 
 output "bridge_ingress_mode" {
@@ -55,22 +55,22 @@ output "bridge_ingress_mode" {
 
 output "bridge_appgw_id" {
   description = "ID of managed Application Gateway when bridge_ingress_mode is appgw_waf."
-  value       = local.is_appgw_ingress_mode ? azurerm_application_gateway.bridge[0].id : null
+  value       = try(azurerm_application_gateway.bridge[0].id, null)
 }
 
 output "bridge_appgw_public_ip" {
   description = "Public IPv4 address of managed Application Gateway when bridge_ingress_mode is appgw_waf."
-  value       = local.is_appgw_ingress_mode ? azurerm_public_ip.bridge_appgw[0].ip_address : null
+  value       = try(azurerm_public_ip.bridge_appgw[0].ip_address, null)
 }
 
 output "bridge_appgw_public_fqdn" {
   description = "Public DNS FQDN of managed Application Gateway when bridge_ingress_mode is appgw_waf."
-  value       = local.is_appgw_ingress_mode ? azurerm_public_ip.bridge_appgw[0].fqdn : null
+  value       = try(azurerm_public_ip.bridge_appgw[0].fqdn, null)
 }
 
 output "bridge_appgw_waf_policy_id" {
   description = "WAF policy ID attached to managed Application Gateway when bridge_ingress_mode is appgw_waf."
-  value       = local.is_appgw_ingress_mode ? azurerm_web_application_firewall_policy.bridge[0].id : null
+  value       = try(azurerm_web_application_firewall_policy.bridge[0].id, null)
 }
 
 output "bridge_vm_id" {
